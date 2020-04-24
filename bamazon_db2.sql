3562b9740eb2721672274a4de4d77d5398ab3c78
KAT <katreadwell1@gmail.com>
2020-04-23 18:24:41 -0700

    removed decimals

--- bamazon_db2.sql
+++ bamazon_db2.sql
@@ -11,17 +11,17 @@ CREATE TABLE products(
     price INTEGER,
     stock_quantity INTEGER,
     PRIMARY KEY (id) 
-)
+);
 
-INSERT INTO products(product_name, department_name, price, stock_quantity) VALUES ('diamond', 'jewelry', 1000, 10)
-INSERT INTO products(product_name, department_name, price, stock_quantity) VALUES ('gold chain', 'jewelry', 150.49, 25)
-INSERT INTO products(product_name, department_name, price, stock_quantity) VALUES ('tie', 'menswear', 100, 100)
-INSERT INTO products(product_name, department_name, price, stock_quantity) VALUES ('cufflinks', 'menswear', 49.99, 50)
-INSERT INTO products(product_name, department_name, price, stock_quantity) VALUES ('teddy bear', 'kids', 20, 1000)
-INSERT INTO products(product_name, department_name, price, stock_quantity) VALUES ('doll', 'kids', 75.95, 4)
-INSERT INTO products(product_name, department_name, price, stock_quantity) VALUES ('chewie toy', 'pets', 9.99, 20)
-INSERT INTO products(product_name, department_name, price, stock_quantity) VALUES ('cat litter', 'pets', 23.99, 574)
-INSERT INTO products(product_name, department_name, price, stock_quantity) VALUES ('knife set', 'housewares', 2000, 5)
-INSERT INTO products(product_name, department_name, price, stock_quantity) VALUES ('dishes', 'housewares', 725.53, 212)
+INSERT INTO products(product_name, department_name, price, stock_quantity) VALUES ('diamond', 'jewelry', 100000, 10);
+INSERT INTO products(product_name, department_name, price, stock_quantity) VALUES ('gold chain', 'jewelry', 15049, 25);
+INSERT INTO products(product_name, department_name, price, stock_quantity) VALUES ('tie', 'menswear', 10000, 100);
+INSERT INTO products(product_name, department_name, price, stock_quantity) VALUES ('cufflinks', 'menswear', 4999, 50);
+INSERT INTO products(product_name, department_name, price, stock_quantity) VALUES ('teddy bear', 'kids', 2000, 1000);
+INSERT INTO products(product_name, department_name, price, stock_quantity) VALUES ('doll', 'kids', 7595, 4);
+INSERT INTO products(product_name, department_name, price, stock_quantity) VALUES ('chewie toy', 'pets', 999, 20);
+INSERT INTO products(product_name, department_name, price, stock_quantity) VALUES ('cat litter', 'pets', 2399, 574);
+INSERT INTO products(product_name, department_name, price, stock_quantity) VALUES ('knife set', 'housewares', 200000, 5);
+INSERT INTO products(product_name, department_name, price, stock_quantity) VALUES ('dishes', 'housewares', 72553, 212);
 
 SELECT * FROM products;
